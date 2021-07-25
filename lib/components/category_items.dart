c4c32399ca7ff05be6b60b359f961eaa1c3f66d7
Kin Yuunan <yuunan.kin@gmail.com>
2021-07-25 09:26:22 +0900

    category item & gradient

---
+++ lib/components/category_items.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Text(this.title),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [this.color.withOpacity(0.7), this.color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)),
+    );
+  }
+}
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import '../components/category_items.dart';
+import '../models/dummy_categories.dart';
 
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-        children: [],
+        children: [
+          ...DUMMY_CATEGORIES.map((e) => CategoryItem(e.title, e.color))
+        ],
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 3 / 2,
